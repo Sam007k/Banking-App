20a8dd487151b5edfd61cecf9bd51f006e198880
Sameer <msameer007k@gmail.com>
2026-07-14 18:29:29 +0530

    implemented-complete-layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,35 @@
-import HeaderBox from '@/components/ui/HeaderBox'
-import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
-import React from 'react'
+import RightSidebar from "@/components/RightSidebar";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  const loggedIn = {firstName: "Sam"}
+  const loggedIn = { firstName: "Sam" ,lastName: "JSM",email: 'contact@bank.com'}
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext = "Access and manage your banking services with ease."
-        />
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your banking services with ease."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        
       </div>
+      <RightSidebar
+          user={loggedIn}
+          transactions={[]}
+          banks={[{currentBalance: 1250.35}, {currentBalance: 2500.00}]}
+        />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
